ec31ccbea22c2bb9a8b25741f584952a43980c18
Riyyi <riyyi3@gmail.com>
2024-02-14 14:23:25 +0100

    Organize project

---
+++ components/stats_component.gd
@@ -0,0 +1,15 @@
+class_name StatsComponent extends Node
+
+# Create the health variable and connect a setter
+@export var health: int = 1:
+	set(value):
+		health = value
+		
+		# Hit
+		health_changed.emit()
+		
+		# Death
+		if health == 0: no_health.emit()
+
+signal health_changed()
+signal no_health()
