3f2a0e54d6d4ba0d65f22015af4eea9359b5bf24
Frank de Weers <fdeweers@gmail.com>
2024-02-16 14:57:14 +0100

    Tweaked enemy pathfinding

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -63,8 +63,8 @@ func _process(_delta) -> void:
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	#if not is_on_floor():
+		#velocity.y -= gravity * delta
 		
 	move_and_slide()
 	
