848dfb508988fe85dc98234ce7b175044624f2ac
Frank de Weers <fdeweers@gmail.com>
2024-02-17 02:07:43 +0100

    update hallways, fullscreen fix

--- components/fullscreen.gd
+++ components/fullscreen.gd
@@ -6,12 +6,14 @@ func _init() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _input(event) -> void:
+	if event.is_action_pressed("ToggleFocus"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if event.is_action_pressed("ToggleFullscreen"):
 		if is_fullscreen:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			is_fullscreen = false
+			print("make windowed")
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			is_fullscreen = true
+			print("make fullscreen")
